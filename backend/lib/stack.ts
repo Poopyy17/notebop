388c9ca7a87d8878d7098df1b50d2912ea26bbd0
Poopy <poopy@Workstation.localdomain>
2026-01-06 18:39:41 +0800

    Fix for stack server app

--- backend/lib/stack.ts
+++ backend/lib/stack.ts
@@ -1,6 +1,6 @@
-import { StackServerApp } from '@stackframe/js'
+import * as StackAuth from '@stackframe/js'
 
-export const stackServerApp = new StackServerApp({
+export const stackServerApp = new StackAuth.StackServerApp({
   projectId: process.env.STACK_PROJECT_ID!,
   publishableClientKey: process.env.STACK_PUBLISHABLE_CLIENT_KEY!,
   secretServerKey: process.env.STACK_SECRET_SERVER_KEY!,
